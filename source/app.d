65c8154055449ad31cabf297db4896741ab5ba02
Robert Schadek <rschadek@symmetryinvestments.com>
2019-09-13 15:29:24 +0800

    rbtree

--- source/app.d
+++ source/app.d
@@ -2,10 +2,14 @@ version(RunTests) {
 void main() {
 	import std.typecons;
 	import betterc.str;
+	import betterc.rbtree;
 	import core.stdc.stdio;
 	printf("libbetterc\n");
 
 	foreach(u; __traits(getUnitTests, betterc.str)) {
+		u();
+	}
+	foreach(u; __traits(getUnitTests, betterc.rbtree)) {
 		u();
 	}
 }
